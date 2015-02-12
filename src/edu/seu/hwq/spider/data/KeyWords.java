1854f97a2499fd17eafc013f39a65c57e2e9bd77
cfxhwq <xmhwq@sina.com>
2015-02-12 19:05:15 +0800

    update keywords'  weight of topic

--- src/edu/seu/hwq/spider/data/KeyWords.java
+++ src/edu/seu/hwq/spider/data/KeyWords.java
@@ -1,14 +1,20 @@
 package edu.seu.hwq.spider.data;
-import java.util.HashMap;
 
-public class KeyWord {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class KeyWords {
 
 	public static HashMap<String, String> keywords = new HashMap<String, String>();
 
-	{
+	public static HashMap<String, Double> topic;
+	static{
 		keywords.put("android", null);
 		{
+			keywords.put("安卓", "android");
 			keywords.put("broadcastreceiver", "android");
+			keywords.put("广播", "android");
 			keywords.put("activity", "android");
 			{
 				keywords.put("oncreate", "activity");
@@ -27,12 +33,16 @@ public class KeyWord {
 				keywords.put("setcontentview", "activity");
 
 			}
+			keywords.put("布局", "android");
 			keywords.put("layout", "android");
 			{
 				keywords.put("linearlayout", "layout");
+				keywords.put("线性布局", "layout");
 				keywords.put("tablelayout", "layout");
 				keywords.put("relativelayout", "layout");
+				keywords.put("相对布局", "layout");
 				keywords.put("framelayout", "layout");
+				keywords.put("帧布局", "layout");
 				keywords.put("absolutelayout", "layout");
 				keywords.put("fragment", "layout");
 
@@ -42,6 +52,7 @@ public class KeyWord {
 				keywords.put("paddingbottom", "layout");
 				keywords.put("padding", "layout");
 				keywords.put("textview", "layout");
+				keywords.put("文本框", "layout");
 				{
 					keywords.put("width", "textview");
 					keywords.put("height", "textview");
@@ -50,9 +61,11 @@ public class KeyWord {
 				}
 				keywords.put("button", "layout");
 				keywords.put("checkbox", "layout");
+				keywords.put("复选框", "layout");
 				keywords.put("button", "layout");
 				{
 					keywords.put("radiobutton", "button");
+					keywords.put("单选框", "button");
 					keywords.put("togglebutton", "button");
 					keywords.put("setonclicklistener", "button");
 				}
@@ -80,40 +93,64 @@ public class KeyWord {
 				keywords.put("stopservice", "service");
 			}
 			keywords.put("contentprovider", "android");
+			keywords.put("开发", "android");
 			keywords.put("develop", "android");
 			{
-				keywords.put("mainfest", "developer");
-				keywords.put("sharedpreference", "developer");
-				keywords.put("asyncplayer", "developer");
-				keywords.put("andrdroidmainfest", "developer");
+				keywords.put("developer", "develop");
+				keywords.put("test", "develop");
+				keywords.put("程序", "develop");
+				keywords.put("开发", "develop");
+				keywords.put("测试", "develop");
+				keywords.put("code", "develop");
+				keywords.put("adt", "develop");
+				keywords.put("gms", "develop");
+				keywords.put("代码", "develop");
+				keywords.put("mainfest", "develop");
+				keywords.put("sharedpreference", "develop");
+				keywords.put("asyncplayer", "develop");
+				keywords.put("andrdroidmainfest", "develop");
 				{
 					keywords.put("permission", "andrdroidmainfest");
+					keywords.put("权限", "andrdroidmainfest");
 				}
-				keywords.put("tabhost", "developer");
-				keywords.put("slidingdrawer", "developer");
-				keywords.put("adapter", "developer");
+				keywords.put("tabhost", "develop");
+				keywords.put("slidingdrawer", "develop");
+				keywords.put("适配器", "develop");
+				keywords.put("adapter", "develop");
 				{
 					keywords.put("baseadapter", "adapter");
 					keywords.put("cursoradapter", "adapter");
 					keywords.put("resourcecursoradapter", "adapter");
 					keywords.put("simplecursoradapter", "adapter");
 				}
-				keywords.put("scroller", "developer");
+				keywords.put("scroller", "develop");
 				{
 					keywords.put("scrollview", "scroller");
 				}
 				keywords.put("gallery", "developer");
+				keywords.put("handler", "developer");
+				{
+					keywords.put("handleMessage", "handler");
+				}
 				keywords.put("asynctask", "developer");
-				keywords.put("asynctask", "developer");
-				keywords.put("asynctask", "developer");
+				{
+					keywords.put("onprogressupdate", "asynctask");
+					keywords.put("onpreexecute", "asynctask");
+					keywords.put("oncancelled", "asynctask");
+					keywords.put("doinbackground", "asynctask");
+					keywords.put("onpostexecute", "asynctask");
+				}
 				keywords.put("listview", "developer");
 				keywords.put("gridview", "developer");
 				keywords.put("edittext", "developer");
-				keywords.put("asynctask", "developer");
 				keywords.put("xml", "developer");
 				keywords.put("eclipse", "developer");
 				keywords.put("sdk", "developer");
 				keywords.put("java", "developer");
+				{
+					keywords.put("thread", "java");
+					keywords.put("线程", "java");
+				}
 				keywords.put("jni", "developer");
 				{
 					keywords.put("opencv", "developer");
@@ -121,6 +158,34 @@ public class KeyWord {
 				}
 			}
 
+		}
+	}
+
+	public static void init() {
+		if (topic != null) {
+			return;
+		}
+		topic = new HashMap<String, Double>();
+		topic.put("android", 1.0);
+		for (Iterator<?> iterator = keywords.entrySet().iterator(); iterator
+				.hasNext();) {
+			Map.Entry<?, ?> entry = (Map.Entry<?, ?>) iterator.next();
+			String key = (String) entry.getKey();
+			String value = (String) entry.getValue();
+			int depth;
+
+			if (!topic.containsKey(key)) {
+				depth = getDepth(key);
+				topic.put(key, Math.pow(0.9, depth));
+				//System.out.println(key+"\t"+topic.get(key));
+				key = value;
+				while (!topic.containsKey(key)){
+					topic.put(key, Math.pow(0.9,-- depth));
+					//System.out.println(key+"\t"+topic.get(key));
+					key = (String) keywords.get(key);
+					//if(!keywords.containsKey(key)){System.out.println(key);}//test whether keywords exit error
+				} 	
+			}
 		}
 	}
 
@@ -133,5 +198,11 @@ public class KeyWord {
 			depth++;
 		}
 		return depth;
+	}
+	public static void main(String[] args) {
+		System.out.println(keywords.size());
+		//System.out.println(KeyWords.getDepth("android"));
+		KeyWords.init();
+		System.out.println(topic.size());
 	}
 }
--- src/edu/seu/hwq/spider/util/VSM.java
+++ src/edu/seu/hwq/spider/util/VSM.java
@@ -1,9 +1,33 @@
 package edu.seu.hwq.spider.util;
 
-
-
+import java.util.HashMap;
 
 public class VSM {
+	static {
+		System.loadLibrary("Keyword");
+	}
+
+	public static double getSource(String content) {
+		double source;
+		HashMap<String, Double> keyword_value = VSM.getKeywords(content);
+		
+		return 0;
+	}
+
+	public static HashMap<String, Double> getKeywords(String content) {
+		String result = Keyword.getKeywords(content);
+		String[] keywords = result.split(",");
+
+		HashMap<String, Double> keyword_value = new HashMap<String, Double>();
+		for (int i = 0; i < keywords.length; i++) {
+			keyword_value.put(
+					keywords[i].substring(0, keywords[i].indexOf('|')), Double
+							.valueOf(keywords[i].substring(keywords[i]
+									.indexOf('|') + 1)));
+		}
+		System.out.println(keyword_value);
+		return keyword_value;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,26 +48,29 @@ public class VSM {
 
 		// interpreter.execfile("/home/mark/test.py");
 
-//		Process proc;
-//		try {
-//			proc = Runtime.getRuntime().exec("python test.py");
-//			BufferedReader in = new BufferedReader(new InputStreamReader(
-//				     proc.getInputStream()));
-//				   String line;
-//				   while ((line = in.readLine()) != null) {
-//				    System.out.println(line);
-//				   }
-//				   in.close();
-//				   proc.waitFor();
-//				   System.out.println("end");
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		//System.out.println( System.getProperty("java.library.path"));
-		System.loadLibrary("Keyword");
-		System.out.println(Keyword.getKeywords("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CTO，走上人生巅峰。"));
-//		System.out.println( System.getProperty("java.library.path"));
+		// Process proc;
+		// try {
+		// proc = Runtime.getRuntime().exec("python test.py");
+		// BufferedReader in = new BufferedReader(new InputStreamReader(
+		// proc.getInputStream()));
+		// String line;
+		// while ((line = in.readLine()) != null) {
+		// System.out.println(line);
+		// }
+		// in.close();
+		// proc.waitFor();
+		// System.out.println("end");
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// System.out.println( System.getProperty("java.library.path"));
+		// System.out.println(Keyword
+		// .getKeywords("我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CTO，走上人生巅峰。"));
+		// System.out.println( System.getProperty("java.library.path"));
+
+		VSM.getSource("在Handler 异步实现时,涉及到 Handler, Looper, Message,Thread四个对象，实现异步的流程是主线程启动Thread（子线程）àthread(子线程)运行并生成Message-àLooper获取Message并传递给HandleràHandler逐个获取Looper中的Message，并进行UI变更。");
 	}
 
 }
